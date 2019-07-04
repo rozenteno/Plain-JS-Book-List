b657d8d571772ebdc6d059390f3e6976cd753ec0
Rodrigo Sebastián Zenteno Martínez <rozenteno@Rodrigos-MacBook-Pro.local>
2019-07-03 23:32:03 -0500

    Added weather icons css for weather widget, geolocation implemented, missing: displaying current temp and location from api response

--- appes6.js
+++ appes6.js
@@ -1,3 +1,52 @@
+// WEATHER LOGIC
+const notificationElement = document.querySelector(".notification");
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+
+const weather = {};
+
+weather.temperature = {
+  unit: "celsius"
+};
+
+const KELVIN = 273;
+const key = "1e90a349f352c2e3bcc0febbd6ad1fa9";
+
+function getWeather(latitude, longitude) {
+  let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+  fetch(api)
+    .then(function(responde) {
+      let data = response.json();
+      return data;
+    })
+    .then(function(data) {
+      weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+      weather.description = data.weather[0].icon;
+      // weather.iconId
+    });
+}
+
+function setPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  getWeather(latitude, longitude);
+}
+
+function showError(error) {
+  notificationElement.style.display = "block";
+  notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(setPosition, showError);
+} else {
+  notificationElement.style.display = "block";
+  notificationElement.innerHTML = "<p>Browser doesn't suppert Geolocation</p>";
+}
+// BOOK LOGIC
+
 class Book {
   constructor(title, author, isbn) {
     this.title = title;
